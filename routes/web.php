d45cae0217c0767055f4bca84d5127118225e678
bintang111005 <bintang1112005@gmail.com>
2026-05-19 12:07:34 +0700

    inventory app project

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,14 @@
 
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+
+Route::resource('categories', CategoryController::class);
 Route::get('/products', [ProductController::class, 'index']);
 Route::get('/insert', [ProductController::class, 'insert']);
 Route::get('/update/{id}', [ProductController::class, 'update']);
